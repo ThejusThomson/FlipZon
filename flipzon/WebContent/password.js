94a644d5034189b8dec4d7f382bc85f8b65d639c
NagaVyshnavi99 <61269574+NagaVyshnavi99@users.noreply.github.com>
2020-04-19 16:47:24 +0530

    Add files via upload

--- flipzon/WebContent/password.js
+++ flipzon/WebContent/password.js
@@ -1,6 +1,4 @@
-/**
- * 
- */function checkPass() {
+function checkPass() {
 var pass1=document.getElementById('pass1');
 var pass2=document.getElementById('pass2');
 var message=document.getElementById('confirmMessage');
@@ -10,12 +8,13 @@ if(pass1.value==pass2.value) {
 pass2.style.backgroundColor=goodColor;
 message.style.color = goodColor;
 message.innerHTML="Password Match!"
+return true;
 }
 else {
 pass2.style.backgroundColor=badColor;
 message.style.color = badColor;
 message.innerHTML="Password Does Not Match!"
-
+return false;
 }
 }
 function checkValid() {
@@ -28,6 +27,7 @@ if (str.match(/[a-z]/g) && str.match(/[A-Z]/g) && str.match(/[0-9]/g) && str.len
    pass1.style.backgroundColor=goodColor;
    msg.innerHTML = "<span style='color: green;'>"+ 
                         "Password accepted</span>" 
+   return true;
 }
                 
    else 
@@ -35,8 +35,31 @@ if (str.match(/[a-z]/g) && str.match(/[A-Z]/g) && str.match(/[0-9]/g) && str.len
                pass1.style.backgroundColor=badColor;
                 msg.innerHTML = "<span style='color: red;'>"+ 
                         "Your password does not follow the password policy</span>" 
+            return false;
   
         } 
       
      
 }
+function checkSubmit()
+{
+var x=checkPass();
+var y=checkValid();
+if(x==true && y==true)
+{
+  return true;
+}else if(x==true && y==false)
+{
+  alert("Please enter the password as per policy");
+  return false;
+}else if(x==false && y==true)
+{
+    alert("Password and confirm Password should match");
+    return false;
+}else
+{
+  alert("Please enter the password correctly");
+  return false;
+}
+}
+   
